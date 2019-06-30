736410cd1af16a4d42f0b9737199e701690c4dd4
Moshe Peres <mosheper99@gmail.com>
2019-06-30 15:20:21 -0400

    added the links when you search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SearchBarComponent} from './search-bar-ingridients/search-bar.component';
 import {SearchResultsIngridientsComponent} from './search-results-ingridients/search-results-ingridients.component';
-// import {SearchResultsRecipiesComponent} from './search-results-recipies/search-results-recipies.component';
+import {SearchResultsRecipiesComponent} from './search-results-recipies/search-results-recipies.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: SearchBarComponent},
   {path: 'ingridientsresult/:ing' , component: SearchResultsIngridientsComponent},
-  // {path: 'recipesresults', component: SearchResultsRecipiesComponent}
+  {path: 'recipesresults', component: SearchResultsRecipiesComponent}
 ];
 
 @NgModule({
